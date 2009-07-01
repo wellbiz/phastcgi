10f4bbb84e21973b1d101421a50b7eebcca2e585
opium <redixin@red.ydk.com.ua>
2009-07-01 16:37:10 +0300

    checking record type

--- phastcgi/fastcgi.php
+++ phastcgi/fastcgi.php
@@ -1,5 +1,5 @@
 <?
-
+# record types
 define(FCGI_BEGIN_REQUEST, 1);
 define(FCGI_ABORT_REQUEST, 2);
 define(FCGI_END_REQUEST, 3);
@@ -13,11 +13,13 @@ define(FCGI_GET_VALUES_RESULT , 10);
 define(FCGI_UNKNOWN_TYPE, 11);
 define(FCGI_MAXTYPE, FCGI_UNKNOWN_TYPE);
 define(FCGI_NULL_REQUEST_ID, 0);
+
 define(FCGI_KEEP_CONN, 1);
 
 define(FCGI_RESPONDER, 1);
 define(FCGI_AUTHORIZER, 2);
 define(FCGI_FILTER, 3);
+
 define(FCGI_REQUEST_COMPLETE, 0);
 define(FCGI_CANT_MPX_CONN, 1);
 define(FCGI_OVERLOADED, 2);
@@ -32,6 +34,40 @@ class FastCGIRecord
         $this->requestId = ord($data[2]) * 256 + ord($data[3]);
         $this->contentLength = ord($data[4]) * 256 + ord($data[5]);
         $this->paddingLength = ord($data[6]);
+
+        if($this->contentLength > 0)
+            $data = socket_read($s, $this->contentLength);
+
+        if($this->type == FCGI_PARAMS)
+        {
+            $offset = 0;
+            while($offset < $this->contentLength)
+            {
+                $namelen = ord($data[$offset++]);
+                if($namelen > 127)
+                {
+                    $namelen = (($namelen & 0x7f) * 16777216) +
+                                (ord($data[$offset++]) * 65536) + 
+                                (ord($data[$offset++]) * 256) + 
+                                ord($data[$offset++]);
+                }
+
+                $valuelen = ord($data[$offset++]);
+                if($valuelen > 127)
+                {
+                    $valuelen = (($valuelen & 0x7f) * 16777216) +
+                                (ord($data[$offset++]) * 65536) + 
+                                (ord($data[$offset++]) * 256) + 
+                                ord($data[$offset++]);
+                }
+
+                $name = substr($data, $offset, $namelen);
+                $offset += $namelen;
+                $value = substr($data, $offset, $valuelen);
+                $offset += $valuelen;
+                $this->params[$name] = $value;
+            }
+        }
     }
 }
 
@@ -41,40 +77,10 @@ class FastCGIRequest
     {
         $rec = new FastCGIRecord($s);
         var_dump($rec);
-
-        $content_length = ord($br[20]) * 256 + ord($br[21]);
-
-        $data = socket_read($s, $content_length);
-        $offset = 0;
-
-        while($offset < $content_length)
-        {
-            $namelen = ord($data[$offset++]);
-            if($namelen > 127)
-            {
-                $namelen = (($namelen & 0x7f) * 16777216) +
-                            (ord($data[$offset++]) * 65536) + 
-                            (ord($data[$offset++]) * 256) + 
-                            ord($data[$offset++]);
-            }
-
-            $valuelen = ord($data[$offset++]);
-            if($valuelen > 127)
-            {
-                $valuelen = (($valuelen & 0x7f) * 16777216) +
-                            (ord($data[$offset++]) * 65536) + 
-                            (ord($data[$offset++]) * 256) + 
-                            ord($data[$offset++]);
-            }
-
-            $name = substr($data, $offset, $namelen);
-            $offset += $namelen;
-            $value = substr($data, $offset, $valuelen);
-            $offset += $valuelen;
-
-            $this->headers[$name] = $value;
-        }
-    }
+        $rec = new FastCGIRecord($s);
+        var_dump($rec);
+     }
 }
+
 
 
