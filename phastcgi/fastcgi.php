df3e2507c389224e814335f7814adaa27346d528
Chirkin Dmitry <josser@josser-mbp.local>
2009-07-02 11:50:40 +0300

    Added Exception error handler; Fixed non-static use of static property;

--- phastcgi/fastcgi.php
+++ phastcgi/fastcgi.php
@@ -1,30 +1,30 @@
 <?
 # record types
-define(FCGI_BEGIN_REQUEST, 1);
-define(FCGI_ABORT_REQUEST, 2);
-define(FCGI_END_REQUEST, 3);
-define(FCGI_PARAMS, 4);
-define(FCGI_STDIN, 5);
-define(FCGI_STDOUT, 6);
-define(FCGI_STDERR, 7);
-define(FCGI_DATA, 8);
-define(FCGI_GET_VALUES, 9);
-define(FCGI_GET_VALUES_RESULT , 10);
-define(FCGI_UNKNOWN_TYPE, 11);
-define(FCGI_MAXTYPE, FCGI_UNKNOWN_TYPE);
-define(FCGI_NULL_REQUEST_ID, 0);
+define('FCGI_BEGIN_REQUEST', 1);
+define('FCGI_ABORT_REQUEST', 2);
+define('FCGI_END_REQUEST', 3);
+define('FCGI_PARAMS', 4);
+define('FCGI_STDIN', 5);
+define('FCGI_STDOUT', 6);
+define('FCGI_STDERR', 7);
+define('FCGI_DATA', 8);
+define('FCGI_GET_VALUES', 9);
+define('FCGI_GET_VALUES_RESULT' , 10);
+define('FCGI_UNKNOWN_TYPE', 11);
+define('FCGI_MAXTYPE', FCGI_UNKNOWN_TYPE);
+define('FCGI_NULL_REQUEST_ID', 0);
 
-define(FCGI_VERSION_1, 1);
-define(FCGI_KEEP_CONN, 1);
+define('FCGI_VERSION_1', 1);
+define('FCGI_KEEP_CONN', 1);
 
-define(FCGI_RESPONDER, 1);
-define(FCGI_AUTHORIZER, 2);
-define(FCGI_FILTER, 3);
+define('FCGI_RESPONDER', 1);
+define('FCGI_AUTHORIZER', 2);
+define('FCGI_FILTER', 3);
 
-define(FCGI_REQUEST_COMPLETE, 0);
-define(FCGI_CANT_MPX_CONN, 1);
-define(FCGI_OVERLOADED, 2);
-define(FCGI_UNKNOWN_ROLE, 3);       
+define('FCGI_REQUEST_COMPLETE', 0);
+define('FCGI_CANT_MPX_CONN', 1);
+define('FCGI_OVERLOADED', 2);
+define('FCGI_UNKNOWN_ROLE', 3);       
 
 class FastCGIRecord
 {
@@ -35,7 +35,7 @@ class FastCGIRecord
 	
     public function __construct($sock)
     {
-		$this->sock = $sock;
+		self::$sock = $sock;
         $this->version = FCGI_VERSION_1;
         $this->contentLengthB0 = 0;
         $this->contentLengthB1 = 0;
@@ -46,14 +46,14 @@ class FastCGIRecord
     }
     public function read()
     {
-        $data = socket_read($this->sock, 8);
+        $data = socket_read(self::$sock, 8);
         $this->type = ord($data[1]);
         $this->requestId = (ord($data[2]) << 8) + ord($data[3]);
         $this->contentLength = (ord($data[4]) << 8) + ord($data[5]);
         $this->paddingLength = ord($data[6]);
 
         if($this->contentLength > 0)
-            $data = socket_read($this->sock, $this->contentLength);
+            $data = socket_read(self::$sock, $this->contentLength);
 
         if($this->type == FCGI_PARAMS)
         {
@@ -89,7 +89,7 @@ class FastCGIRecord
 
     public function write()
     {
-        socket_write($this->sock, 
+        socket_write(self::$sock, 
             chr($this->version).
             chr($this->type)."\x00\x00".
             chr($this->contentLengthB1).
@@ -97,7 +97,7 @@ class FastCGIRecord
             chr($this->paddingLength).
             0x00);
         if($this->contentLength > 0)
-            socket_write($this->sock, $this->data);
+            socket_write(self::$sock, $this->data);
     }
 
     public function add_data($data)
@@ -120,6 +120,3 @@ class FastCGIRequest
         } while ($rec->type != FCGI_STDIN);
     }
 }
-
-
-
